a89692af8926991af363bace7b0900a107e28a18
Casperteam <84174959+CasperTeam@users.noreply.github.com>
2022-01-09 11:51:25 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/AsmSafone/MusicPlayer /MusicPlayer
-echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
+git clone https://github.com/casperteam/qp /qp ">> INSTALLING REQUIREMENTS..."
+cd /qp
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
